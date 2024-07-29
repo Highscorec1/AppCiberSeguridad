3667009100fbb9a2c875495f96d3c3a202e20920
HighMac <geovajoel20@gmail.com>
2024-07-29 15:33:51 -0700

    MVVM modelo definido y fragments iniciados

---
+++ app/src/main/java/com/example/aplicacionhack/ui/informacion/informacionFragment.kt
@@ -0,0 +1,24 @@
+package com.example.aplicacionhack.ui.informacion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.aplicacionhack.R
+import com.example.aplicacionhack.databinding.FragmentInformacionBinding
+
+
+class informacionFragment : Fragment() {
+
+    private var _binding: FragmentInformacionBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentInformacionBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
